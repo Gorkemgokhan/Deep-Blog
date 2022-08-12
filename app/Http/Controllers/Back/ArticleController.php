97ee709241f5b652173045dfdd297879ea0a42f1
Gorkemgokhan <110126902+Gorkemgokhan@users.noreply.github.com>
2022-08-12 17:55:12 +0300

    güncel

---
+++ app/Http/Controllers/Back/ArticleController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\Back;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Article;
+use App\Models\Category;
+use Illuminate\Support\Str;
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $articles=Article::orderBy("created_at","ASC")->get();
+        return view("back\articiles\index",compact("articles"));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories=Category::all();
+        return view("back\articiles\create",compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+           "title"=>"min:3",
+            "image"=>"required|image|mimes:jpeg,png,jpg|max:100"
+        ]);
+
+        $article=new Article;
+        $article->title=$request->title;
+        $article->category_id=$request->category;
+        $article->content=$request->content;
+        $article->slug=Str::slug($request->title);
+        if($request->hasFile("image")){
+            $imageName=Str::slug("$request->title").".".$request->image->getClientOriginalExtension();
+            $request->image->move(public_path("uploads"),$imageName);
+            $article->image="uploads/".$imageName;
+        }
+        $article->save();
+        return redirect()->route("admin.makaleler.create");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return $id;
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return $id."edit";
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
